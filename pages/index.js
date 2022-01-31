41417dcf4873f4a9f5fa8360f9098ca66cb346f0
msko <kms89123@gmail.com>
2022-01-31 21:44:10 +0900

    Rollback Link Tag

--- pages/index.js
+++ pages/index.js
@@ -26,10 +26,7 @@ export default function Home({ allPostsData }) {
         </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
-          <Link href='https://nextjs.org/learn'>
-            <a>our Next.js tutorial.</a>
-          </Link>
-          )
+          <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
