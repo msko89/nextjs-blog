274c0c50548f1dc246520c4628572ffb034acea4
msko <kms89123@gmail.com>
2022-01-31 21:39:56 +0900

    Update Link Tag

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/posts";
-import Link from "next/link";
-import Date from "../components/date";
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -25,8 +25,11 @@ export default function Home({ allPostsData }) {
           Hello, I'm <strong>Msko.</strong> I'm software enginer.
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <Link href='https://nextjs.org/learn'>
+            <a>our Next.js tutorial.</a>
+          </Link>
+          )
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
